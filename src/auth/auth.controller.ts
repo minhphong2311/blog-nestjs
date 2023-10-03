bed3065b9f46a1afc2aa6f8758aa0ecad0fa2ffc
minhphong2311 <minhphong2311@gmail.com>
2023-10-03 16:06:31 +0700

    #03: JWT Authentication, Login, Refresh Token API, Pipe

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { AuthService } from './auth.service';
 import { User } from 'src/user/entities/user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,5 +19,20 @@ export class AuthController {
     console.log('register api');
     console.log(registerUserDto);
     return this.authService.register(registerUserDto);
+  }
+
+  @Post('login')
+  @UsePipes(ValidationPipe)
+  login(@Body() loginUserDto: LoginUserDto): Promise<any> {
+    console.log('login');
+    console.log(loginUserDto);
+
+    return this.authService.login(loginUserDto);
+  }
+
+  @Post('refresh-token')
+  refreshToken(@Body() { refresh_token }): Promise<any> {
+    console.log('refresh token api');
+    return this.authService.refreshToken(refresh_token);
   }
 }
