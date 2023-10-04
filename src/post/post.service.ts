766e512cb51eb7aef6112d0863be4f1d00062be0
minhphong2311 <minhphong2311@gmail.com>
2023-10-04 10:10:44 +0700

    #10: Filter Post by Category, Entity Relations

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,9 +1,9 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { Post } from './entities/post.entity';
-import { User } from 'src/user/entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeleteResult, Like, Repository, UpdateResult } from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
 import { FilterPostDto } from './dto/filter-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 
@@ -13,12 +13,14 @@ export class PostService {
     @InjectRepository(User) private userRepository: Repository<User>,
     @InjectRepository(Post) private postRepository: Repository<Post>,
   ) {}
-
   async create(userId: number, createPostDto: CreatePostDto): Promise<Post> {
     const user = await this.userRepository.findOneBy({ id: userId });
 
     try {
-      const res = await this.postRepository.save({ ...createPostDto, user });
+      const res = await this.postRepository.save({
+        ...createPostDto,
+        user,
+      });
 
       return await this.postRepository.findOneBy({ id: res.id });
     } catch (error) {
@@ -30,15 +32,22 @@ export class PostService {
     const items_per_page = Number(query.items_per_page) || 10;
     const page = Number(query.page) || 1;
     const search = query.search || '';
+    const category = Number(query.category) || null;
 
     const skip = (page - 1) * items_per_page;
     const [res, total] = await this.postRepository.findAndCount({
       where: [
         {
           title: Like('%' + search + '%'),
+          category: {
+            id: category,
+          },
         },
         {
           description: Like('%' + search + '%'),
+          category: {
+            id: category,
+          },
         },
       ],
       order: { created_at: 'DESC' },
@@ -46,8 +55,13 @@ export class PostService {
       skip: skip,
       relations: {
         user: true,
+        category: true,
       },
       select: {
+        category: {
+          id: true,
+          name: true,
+        },
         user: {
           id: true,
           first_name: true,
@@ -75,8 +89,12 @@ export class PostService {
   async findDetail(id: number): Promise<Post> {
     return await this.postRepository.findOne({
       where: { id },
-      relations: ['user'],
+      relations: ['user', 'category'],
       select: {
+        category: {
+          id: true,
+          name: true,
+        },
         user: {
           id: true,
           first_name: true,
